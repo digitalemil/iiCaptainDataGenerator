01ad6092b18d3630ce6450f7e21e6592e229a847
digitalemil <digitalemil@googlemail.com>
2014-01-10 16:41:23 +0100

    added refine.sh

---
+++ refine.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pig -useHCatalog -x mapreduce -e "storeData = LOAD 'default.rawstoredata' USING org.apache.hcatalog.pig.HCatLoader(); refinedStoreData= FOREACH storeData GENERATE appstore, startdate, MilliSecondsBetween(ToDate(startdate), ToDate('1970-01-01', 'yyyy-mm-dd'))-978307200000L AS geostartdate, MilliSecondsBetween(ToDate(startdate), ToDate('1970-01-01', 'yyyy-mm-dd'))-978307200000L+86400000L AS geoenddate, country, unitsdownloads, unitsupdates, unitsrefunds, unitspromotions, revenuedownloads, revenueupdates, revenuerefunds, revenuepromotions; DUMP refinedStoreData; STORE refinedStoreData INTO 'default.refinedstoredata' USING org.apache.hcatalog.pig.HCatStorer();"
+
+ exit 0
